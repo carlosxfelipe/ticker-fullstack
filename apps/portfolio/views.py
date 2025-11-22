2623a08e2ba31ae44a6c1e90d93c8b9691b32edc
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2025-11-22 08:45:14 -0300

    add dynamic asset rendering to portfolio home view

--- apps/portfolio/views.py
+++ apps/portfolio/views.py
@@ -4,4 +4,17 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, "portfolio/portfolio.html")
+    user = request.user
+    assets = []
+    if user.is_authenticated:
+        assets = user.assets.all()
+    labels = [asset.ticker for asset in assets]
+    values = [asset.quantity for asset in assets]
+    return render(
+        request,
+        "portfolio/portfolio.html",
+        {
+            "labels": labels,
+            "values": values,
+        },
+    )
