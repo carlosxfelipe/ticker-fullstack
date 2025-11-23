f92bd629710ea376fa8741c15a6fb0bcd5a6db92
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2025-11-23 17:55:37 -0300

    style: ensure ticker is stored in uppercase when saving asset

--- apps/portfolio/models.py
+++ apps/portfolio/models.py
@@ -13,5 +13,10 @@ class Asset(models.Model):
     average_price = models.FloatField()
     current_price = models.FloatField(null=True, blank=True)
 
+    def save(self, *args, **kwargs):
+        if self.ticker:
+            self.ticker = self.ticker.upper()
+        super().save(*args, **kwargs)
+
     def __str__(self):
         return f"{self.ticker} ({self.user.username})"
